493e5b440932f99f91eafae33c613b12b35910c8
16flotho <tom.florian@hotmail.com>
2022-07-06 15:47:32 +0200

    feat: improve abfahrt extension

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
 var buttons = document.querySelectorAll('button[type="submit"]')
+var inputs = document.querySelectorAll('input[type="submit"]')
 for(let i = 0; i < buttons.length; i++) {
     buttons[i].title = buttons[i].innerText;
-    buttons[i].innerText = "Abfahrt!🚗💨";  
+    buttons[i].innerText = buttons[i].clientWidth < 50 ? "🚗" : "Abfahrt!🚗💨";  
 }
+inputs.forEach(input => {
+    input.title = input.value;
+    input.value = "Abfahrt!🚗💨";  
+})
 
 
